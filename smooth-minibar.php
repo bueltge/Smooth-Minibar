d15899b3e12603d3cdb098135def3848e6f8cf83
Frank Bültge <frank@bueltge.de>
2011-04-04 22:25:20 +0200

    small changes on phpdoc

--- smooth-minibar.php
+++ smooth-minibar.php
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 class Smooth_Minibar {
 	
 	/**
-	 * construct
+	 * constructer
 	 * 
 	 * @uses add_filter, add_action
 	 * @access public
