f6b0f33dea27547881500b2080e695330245bfa9
Frank Bültge <frank@bueltge.de>
2011-04-08 23:11:55 +0200

    add filter to read buttons

--- inc/class.option-table-meta-box.php
+++ inc/class.option-table-meta-box.php
@@ -20,6 +20,8 @@ class Option_Table_Meta_Box {
 		$this->textdomain = $this->smooth_minibar->get_textdomain();
 		$this->pagehook = 'settings_page_' . $this->textdomain . '-settings';
 		
+		add_filter( 'smooth_minibar_dblclick_buttons', array( $this, 'get_smooth_minibar_dblclick_buttons' ), 20, 1 );
+		
 		add_meta_box( 
 			$this->textdomain . '-sidebox-option',
 			__( 'Options', $this->textdomain ),
@@ -30,6 +32,11 @@ class Option_Table_Meta_Box {
 		);
 	}
 	
+	public function get_smooth_minibar_dblclick_buttons($dblclick_buttons) {
+		echo 'TEST';
+		var_dump($dblclick_buttons);exit;
+	}
+	
 	/**
 	 * below the metaboxes for settings
 	 * siede, normal, additional
@@ -38,17 +45,20 @@ class Option_Table_Meta_Box {
 		?>
 		<table class="widefat" cellspacing="0">
 		<?php
+		$importers = array( 'test' => array(1, 2, 3, 0), 'test2' => array(5, 6, 7, 9));
 			$style = '';
 			foreach ($importers as $id => $data) {
 				$style = ('class="alternate"' == $style || 'class="alternate active"' == $style) ? '' : 'alternate';
 				
 				if ($style != '')
-					$style = 'class="'.$style.'"';
-				echo "
-					<tr $style>
-						<td class='import-system row-title'>$action</td>
-						<td class='desc'>{$data[1]}</td>
-					</tr>";
+					$style = 'class="' . $style . '"';
+				echo '
+					<tr ' . $style . '>
+						<td class="row-title">' . $id . '</td>
+						<td>' . $data[1] . '</td>
+						<td>' . $data[2] . '</td>
+						<td>' . $data[3] . '</td>
+					</tr>';
 			}
 		?>
 		</table>
